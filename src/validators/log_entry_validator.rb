151c461d8129b17c3ec0fec50aff0b105cb1ee02
Aliexandr Andrade <aliexandr.andrade@spaceos.io>
2021-03-21 16:48:21 +0300

    log entry validator

---
+++ src/validators/log_entry_validator.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative '../result'
+
+class LogEntryValidator
+  def initialize(line)
+    @line = line
+  end
+
+  def validate
+    if stripped_line.empty?
+      return Result.failure('blank line exists')
+    end
+
+    unless stripped_line =~ /\s/
+      return Result.failure('log entry incorrect format')
+    end
+
+    Result.success
+  end
+
+  private
+
+  def stripped_line
+    @stripped_line ||= line.strip
+  end
+
+  attr_reader :line
+end
